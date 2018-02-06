9a61e23f8d7d497a9ced2c36f756202528cfc17e
Benny Souriyadeth <bennys@uw.edu>
2018-02-05 17:45:10 -0800

    Initial Commit

---
+++ tableviewex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableviewex
+//
+//  Created by Benny on 2/5/18.
+//  Copyright © 2018 Benny Souriyadeth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
